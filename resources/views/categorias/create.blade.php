6e96ec780054f02f9ba0c2d1e2a33a49ec20d8f9
Kenny Rodriguez <kennyre8@gmail.com>
2018-10-30 04:06:16 -0500

    make it better

--- resources/views/categorias/create.blade.php
+++ resources/views/categorias/create.blade.php
@@ -14,6 +14,32 @@
     @else
 	    <form method="post" action="{{ route('categorias.store') }}">
 
+	    	{{ csrf_field() }}
+
+	    	<div>
+		    	<label for="titulo_categoria">
+		    		Título Categoría
+		    		<input type="text" name="titulo_categoria" value="{{ old('titulo_categoria') }}">
+
+		    		{!! $errors->first('titulo_categoria','<span class=error>:message</span>') !!}
+
+		    	</label>
+		    </div>
+		    <br>
+
+	    	<div>
+		    	<label for="desc_categoria">
+		    		Descripción Categoría
+		    		<input type="text" name="desc_categoria" value="{{ old('desc_categoria') }}">
+
+					{!! $errors->first('desc_categoria','<span class=error>:message</span>') !!}
+
+		    	</label>
+	    	</div>
+	    	<br>
+
+	    	<input type="submit" value="Crear Categoria">
+
 	    </form>
 
 	@endif
